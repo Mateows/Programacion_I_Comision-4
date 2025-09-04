86d266a879b4149bedfc8de044954268c1030086
Mateows <mateoliendo633@gmail.com>
2025-09-04 11:27:07 -0300

    Terminado ejercicio con bucles

--- Ejercicios_Clase/Clase4-9-2025.py
+++ Ejercicios_Clase/Clase4-9-2025.py
@@ -1,12 +1,17 @@
-#Crear un programa donde se creen nuevas patentes
+# Hacer un programa que el usuario introduzca un numero para saber en cuanto se va a incrementar la patente
+# por ejemplo si el usuario ingresa 1000 la patente debe de quedar como AAB000
 
-patente_letra=["A","B","C"]
+patente_letra="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+n_patente= int(input("Ingrese el nùmero que quiera para generar una patente\n "))
+contador = 0
 for i in (patente_letra):
     for j in (patente_letra):
         for k in (patente_letra):
-            for x in range(2):
-                for y in range(2):
-                    for z in range(2):
-                        print(i,j,k,"", x,y,z)  
-
+            for x in range(10):
+                for y in range(10):
+                    for z in range(10):
+                        if contador == n_patente:
+                            print(i,j,k,"",x,y,z)
+                            exit()
+                        contador += 1
