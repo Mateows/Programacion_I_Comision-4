83303269b94997b22e38df3d37f0eee9e03b6926
Mateows <54292979+Mateows@users.noreply.github.com>
2025-09-04 09:37:55 -0300

    Add files via upload

---
+++ Ejercicios_Clase/Clase4-9-2025.py
@@ -0,0 +1,12 @@
+#Crear un programa donde se creen nuevas patentes
+
+patente_letra=["A","B","C"]
+
+for i in (patente_letra):
+    for j in (patente_letra):
+        for k in (patente_letra):
+            for x in range(2):
+                for y in range(2):
+                    for z in range(2):
+                        print(i,j,k,"", x,y,z)  
+
